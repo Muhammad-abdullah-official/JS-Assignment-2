518bb910d9a354a018f56d30a6de82a840bc640f
Muhammad-abdullah-official <amabdullah643@gmail.com>
2024-05-24 18:54:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,160 @@
+//Q1
+// var a = 13,
+//   b = 9;
+// var sum = a + b;
+// alert(`Sum of ${a} and ${b} is ${sum}`);
+
+//Q2
+// var a = 13,
+//   b = 9;
+// var sub = a - b;
+// alert(`Subtraction of ${a} and ${b} is ${sub}`);
+
+// var a = 13,
+//   b = 9;
+// var mul = a * b;
+// alert(`Multiplication of ${a} and ${b} is ${mul}`);
+
+// var a = 13,
+//   b = 9;
+// var div = a / b;
+// alert(`Division of ${a} and ${b} is ${div}`);
+
+// var a = 13,
+//   b = 9;
+// var mod = a % b;
+// alert(`Modulus of ${a} and ${b} is ${mod}`);
+
+//Q3
+// var num;
+// alert(`Value after variable decleration is: ${num}`);
+// num = 3;
+
+// alert(`Initial value: ${num}`);
+// num++;
+
+// alert(`Value after increment is : ${num}`);
+
+// num += 7;
+
+// alert(`Value after addition is: ${num}`);
+
+// num--;
+// alert(`Value after decrement is: ${num}`);
+
+// var rem = num % 3;
+// alert(`The remainder is: ${rem}`);
+
+//Q4
+// var movieprice = 600;
+// alert('Price for 1 ticket for movie is ' + movieprice);
+// alert(`Price for 5 tickets for movie is ${movieprice * 5}`);
+
+// //Q5
+// var num = 4;
+// document.write(`${num} x 1 = ${num * 1}`);
+// document.write(`${num} x 2 = ${num * 2}`);
+// document.write(`${num} x 3 = ${num * 3}`);
+// document.write(`${num} x 4 = ${num * 4}`);
+// document.write(`${num} x 5 = ${num * 5}`);
+// document.write(`${num} x 6 = ${num * 6}`);
+// document.write(`${num} x 7 = ${num * 7}`);
+// document.write(`${num} x 8 = ${num * 8}`);
+// document.write(`${num} x 9 = ${num * 9}`);
+// document.write(`${num} x 10 = ${num * 10}`);
+
+//Q6
+// var cel = 12;
+// var far;
+// document.write(`cel C is ${cel * (9 / 5) + 32}F`);
+
+// far = 68;
+// document.write(`far F is ${(far - 32) * (5 / 9)}C`);
+
+// var pkritem1 = 560;
+// var pkritem2 = 200;
+// var qtyitem1 = 2;
+// var qtyitem2 = 5;
+// var shpchrg = 150;
+// var total = pkritem1 * qtyitem1 + pkritem2 * qtyitem2 + shpchrg;
+// document.write(`Price of item 1 is ${pkritem1}`);
+// document.write(`Quantity of item 1 is ${qtyitem1}`);
+// document.write(`Price of item 2 is ${pkritem2}`);
+// document.write(`Quantity of item 1 is ${qtyitem2}`);
+// document.write(`Shipping charges ${shpchrg}`);
+// document.write(`Total cost of your order is ${total}`);
+
+// document.write('<h1>Marks Sheet </h1>');
+// var totalmarks = 1200;
+// var marksobtained = 900;
+// var result = (marksobtained * 100) / totalmarks;
+
+// document.write(`<h2> Total Marks : ${totalmarks}</h2>`);
+// document.write(`<h2> Obtained Marks : ${marksobtained}</h2>`);
+// document.write(`<h2> Percentage : ${result}%</h2>`);
+
+// var dollar = 104,
+//   riyal = 28;
+// var total = dollar * 10 + riyal * 25;
+// document.write(`<h1> Total currency in PKR : ${total}</h1>`);
+
+// var num = ((2 + 5) * 10) / 2;
+// document.write(`${num}`);
+
+// var byear = 2000;
+// var cyear = 2023;
+
+// document.write(`Current Year : ${cyear}`);
+// document.write(`Birth Year : ${byear}`);
+// document.write(`They are either ${cyear - byear} or ${cyear - byear + 1}`);
+
+// var radius = 12;
+// document.write(`The radius is : ${radius}`);
+
+// var circum = 2 * 3.142 * radius;
+// document.write(`The circumference is : ${circum}`);
+
+// var area = 3.142 * (radius * radius);
+// document.write(`The circumference is : ${area}`);
+
+// var a = 10;
+
+// document.write(`Value of a is: ${a}`);
+// ++a;
+// document.write(`Value of ++a is: ${a}`);
+// a++;
+// document.write(`Value of a++ is: ${a}`);
+// --a;
+// document.write(`Value of --a is: ${a}`);
+// a--;
+// document.write(`Value of a-- is: ${a}`);
+
+// var a = 2,
+//   b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(`Result ${result}`);
+
+// --a: Pre-decrement a, which makes a equal to 1.
+// --b: Pre-decrement b, which makes b equal to 0.
+// ++b: Pre-increment b, which makes b equal to 1.
+// b--: Post-decrement b, but in this expression, b is still 1, and then it gets decremented to 0.
+// Now, let's calculate the expression step by step:
+
+// --a (1) - --b (0) + ++b (1) + b-- (1)
+// So the expression becomes:
+
+// 1 - 0 + 1 + 1 = 3
+
+// var snack = 'Donuts';
+// var cage = 20;
+// var eage = 63;
+// var snackprdar = 3;
+// var result = (eage - cage) * snackprdar;
+// document.write(`Favourite snack ${snack}`);
+// document.write(`Current age ${cage}`);
+// document.write(`Estimated Maximum Age ${eage}`);
+// document.write(`Amounts of snack per day ${snackprdar}`);
+
+// document.write(
+//   `You will need ${result} ${snack} to last until the ripe old age of ${eage}`
+// );
